70054dda39c6eff841760411bf922600580ae754
Daniel Salgado <dansalgaado@gmail.com>
2025-06-04 19:12:58 -0300

    try to fix Cannot find name do useProducts

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import path from "path";
+import react from "@vitejs/plugin-react";
+import tsconfigPaths from "vite-tsconfig-paths";
+import { defineConfig } from "vite";
+export default defineConfig({
+    plugins: [
+        react(),
+        tsconfigPaths(), // faz o Vite respeitar "paths" do tsconfig.json
+    ],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
